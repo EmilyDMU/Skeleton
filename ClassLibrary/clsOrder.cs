8541c48ffe74918dda79ec752dcb14cf4ee140a1
Michael Kyeremeh <P2711736@my365.dmu.ac.uk>
2024-05-17 15:01:38 +0100

    Test done

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public bool OrderStatus { get; set; }
+        public float TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int StaffId { get; set; }
+        public int CustomerId { get; set; }
+        public int OrderId { get; set; }
+        public int ShoeId { get; set; }
+        public string Note { get; set; }
+    }
+}
