1babeabf3be2e01509b95c77f9744c77b8245b2e
Shammah <P2413877@my365.dmu.ac.uk>
2024-05-28 06:46:11 +0100

    Did pratctical 6 and 7 in the correct solution. Added all tests for staff properties

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public DateTime DateAdded { get; set; }
+        public string StaffDepartment { get; set; }
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public bool StaffPermission { get; set; }
+        public string StaffRole { get; set; }
+        public string StaffStatus { get; set; }
+    }
+}
